d9f630dcf50a29fa34925c7d44766d971e887756
Vidhi2507 <vidhibhanushali2507@gmail.com>
2026-04-04 17:47:05 +0530

    Education Details

--- backend/classes.py
+++ backend/classes.py
@@ -5,3 +5,13 @@ class User(BaseModel):
     username: Optional[str] = None
     email: EmailStr
     password: str
+
+class UserCareerInfo(BaseModel):
+    username: Optional[str] = None
+    email: EmailStr
+    Highesteducation: Optional[str] = None
+    graduation_year: Optional[int] = None
+    current_role: Optional[str] = None
+    years_experience: Optional[int] = None
+    skills: Optional[list[str]] = None
+    career_goals: Optional[str] = None
